3c3c4e218c5bab435a4284c56cdeae5c7d1a301d
core <core@e3t.cc>
2025-02-26 09:51:15 -0500

    feat: reworked session types main page

--- src/routes/(authed)/dash/types/createSchema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const createSchema = z.object({
-	name: z.string(),
-	duration: z.number(),
-	category: z.string(),
-	order: z.number()
-});
-export type CreateSchema = typeof createSchema;
